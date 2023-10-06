d85f752715da6a64d22d772f48362da2bbe90674
Satrio Agung Eko Nugroho_2215101063 <129135406+Satrioagungekonugroho123@users.noreply.github.com>
2023-10-06 19:35:36 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Weather msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+
+</style>
